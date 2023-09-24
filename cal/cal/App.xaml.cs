bdf330b928ec0d07b61c8bece797099217762796
Julio Cesar <juliocesarmartinez1344@gmail.com>
2023-09-23 23:14:25 -0500

    Add project files.

---
+++ cal/cal/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace cal
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new cal();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
